589ea82b8e57204f4d3166812b4490242a5ce9d6
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2023-04-16 07:32:54 +0900

    fix: image change v2

--- packages/site/src/components/SnapLogo.tsx
+++ packages/site/src/components/SnapLogo.tsx
@@ -2,7 +2,7 @@ export const SnapLogo = ({ color, size }: { color: string; size: number }) => (
   <img
     width={size}
     height={size}
-    src="/assets/SocialSecuritySnap.png"
+    src="../assets/SocialSecuritySnap.png"
     alt="SocialSecuritySnap"
     style={{ fill: color }}
   />
