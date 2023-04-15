73fe6a05964d7a0ec090442527d241587523c95d
luka1104 <skyward.luke11@gmail.com>
2023-04-15 13:04:21 +0900

    Add jose dependency and WorldCoin Client ID to server package

---
+++ packages/server/api/worldcoin/verify.ts
@@ -0,0 +1,24 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import * as jose from 'jose';
+
+const verify = async (token: string) => {
+  const JWKS = jose.createRemoteJWKSet(
+    new URL('https://id.worldcoin.org/jwks.json'),
+  );
+  // @ts-ignore
+  const { payload, header } = await jose.jwtVerify(token, JWKS, {
+    issuer: 'https://id.worldcoin.org',
+    aud: process.env.WORLD_COIN_CLIENT_ID,
+  });
+  console.log(payload, header);
+
+  return payload;
+};
+
+const handler = async (req: VercelRequest, res: VercelResponse) => {
+  const { token } = req.body;
+  const result = await verify(token);
+  res.status(200).json({ result });
+};
+
+export default handler;
