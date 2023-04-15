c8d9e33453c67db2a694cc2cf275a0fa7f031751
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2023-04-16 07:47:01 +0900

    fix: image reve

--- packages/site/gatsby-config.ts
+++ packages/site/gatsby-config.ts
@@ -11,7 +11,7 @@ const config: GatsbyConfig = {
       resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'Template Snap',
-        icon: 'src/assets/SocialSecuritySnap.png',
+        icon: 'src/assets/SocialSecuritySnap.svg',
         theme_color: '#6F4CFF',
         background_color: '#FFFFFF',
         display: 'standalone',
