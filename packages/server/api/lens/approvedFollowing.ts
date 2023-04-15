ab5348426d5498a752e11c971c02ff3f4a03f0a0
yusakapon <>
2023-04-15 17:47:55 +0900

    [Fix] approvedFollowing

--- packages/server/api/lens/approvedFollowing.ts
+++ packages/server/api/lens/approvedFollowing.ts
@@ -2,10 +2,20 @@ import { apolloClient } from './utils/apollo-client';
 import { FollowingDocument, FollowingRequest } from './utils/graphql/generated';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { ethers } from 'ethers';
+import axios from 'axios';
 
 type Data = {
   data: any;
 };
+type ABI = Array<{
+  type?: string;
+  name?: string;
+  inputs?: Array<unknown>;
+  outputs?: Array<unknown>;
+  stateMutability?: string;
+  anonymous?: boolean;
+  constant?: boolean;
+}>;
 
 export default async function handler(
   req: VercelRequest,
@@ -37,10 +47,12 @@ const getApprovedAddressList = async (
   req: VercelRequest,
   res: VercelResponse,
 ) => {
-  const { walletAddress, contractAddress, operatorAddress, chainId } = req.query;
+  const { walletAddress, contractAddress, inputData, chainId } = req.query;
   const result = await getFollowingRequest({
     address: walletAddress,
   });
+
+
   console.log('following: result', result);
 
   const ownedByAddresses = result.items.map((item:any) =>  {
@@ -48,8 +60,23 @@ const getApprovedAddressList = async (
   });
   console.log(ownedByAddresses);
 
-  const approvedAddressList =  await checkIsApprovedForAll(contractAddress, ownedByAddresses, operatorAddress, Number(chainId));
 
+  let ResponseData = '';
+  const contractDetails = await getContractDetails(Number(chainId), contractAddress as string);
+    if (!contractDetails) {
+      ResponseData += '【WARNING】 The safety of the function you are trying to execute cannot be confirmed because it has not verified.';
+      return ResponseData;
+    }
+
+    const contractFunctionDetails = await getContractFunctionDetails(inputData as string, contractDetails.abi);
+
+    const functionName = contractFunctionDetails.functionName;
+    const functionArgs = contractFunctionDetails.functionArgs;
+
+    let approvedAddressList = [];
+    if (functionName === 'setApprovalForAll') {
+      approvedAddressList =  await checkIsApprovedForAll(contractAddress, ownedByAddresses, functionArgs[0], Number(chainId));
+    }
   res.status(200).json({ data: approvedAddressList });
 
   return;
@@ -66,7 +93,7 @@ const checkIsApprovedForAll = async (contractAddress:any, ownerAddresses: any, o
   for (const ownerAddress of ownerAddresses) {
     const isApproved = await erc721Contract.isApprovedForAll(ownerAddress.walletAddress, operatorAddress);
     if (isApproved) {
-      approvedForAllAddress.push(ownerAddress.hundle);
+      approvedForAllAddress.push(ownerAddress.hundle as never);
     }
   }
 
@@ -92,5 +119,92 @@ const getRpcUrl = (
       console.log('ChainId not supported.')
       return false;
     }
+  }
+}
+
+/**
+ * Get Construct Details
+ */
+const getContractDetails = async (
+  chainId: number,
+  contractAddress: string
+) => {
+  const apiUri = getContractDetailsApiEndpoint(chainId, contractAddress);
+  if (!apiUri) {
+    return false;
+  }
+
+  const response = await axios.get(apiUri);
+  const result = response.data.result;
+  if (!(result && result[0])) {
+    return false;
+  }
+
+  const contractName = result[0].ContractName;
+  const contractCode = result[0].SourceCode;
+  const abi = result[0].ABI;
+  if (!contractName || !contractCode || !abi) {
+    return false;
+  }
+
+  return {
+    contractName: contractName,
+    contractCode: contractCode,
+    abi: abi
+  };
+}
+
+/**
+ * Get the API Endpoint for obtaining detailed information on the construct
+ */
+const getContractDetailsApiEndpoint = (
+  chainId: number,
+  contractAddress: string
+) => {
+  switch (chainId) {
+    case 1: { // Eth Mainnet
+      let apiKey = process.env.ETHERSCAN_API_KEY as string;
+      return `https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${apiKey}`;
+    }
+    case 5: { // Goerli Testnet
+      let apiKey = process.env.ETHERSCAN_API_KEY as string;
+      return `https://api-goerli.etherscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${apiKey}`;
+    }
+    // case 59140: { // Linea Testnet
+    //   return `https://explorer.goerli.linea.build/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${apiKey}`;
+    // }
+    default: {
+      console.log('ChainId not supported.')
+      return false;
+    }
+  }
+}
+
+
+/**
+ * Get the function name, arguments, and ABI of the function
+ * to be executed based on the contract function signature
+ */
+const getContractFunctionDetails = (
+  inputData: string,
+  contractABI: string
+) => {
+  const contractInterface = new ethers.Interface(contractABI);
+  const decodedFunctionData = contractInterface.parseTransaction({ data: inputData });
+  const functionName = (decodedFunctionData as ethers.TransactionDescription).name;
+  const functionArgs = (decodedFunctionData as ethers.TransactionDescription).args;
+
+  // Get the ABI of the executed function
+  const arrContractABI = JSON.parse(contractABI) as ABI;
+  let functionAbi = arrContractABI.find((item) => item.type === "function" && item.name === functionName);
+  if (!functionAbi) {
+    console.log(`Function '${functionAbi}' not found in ABI.`)
+    functionAbi = {};
+  }
+
+  return {
+    functionName: functionName,
+    functionArgs: functionArgs,
+    functionAbi: functionAbi,
   }
 }
